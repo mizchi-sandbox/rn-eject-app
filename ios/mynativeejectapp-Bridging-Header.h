846f46701a3380fd3580961586917e11a96773d2
mizchi <miz404@gmail.com>
2017-06-27 23:18:19 +0900

    Rewrite with Swift

---
+++ ios/mynativeejectapp-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import <React/RCTBundleURLProvider.h>
+#import <React/RCTRootView.h>
+#import <UIKit/UIKit.h>
+//#import "RCTBridgeModule.h"
+//#import "RCTBridge.h"
+//#import "RCTEventDispatcher.h"
+//#import "RCTRootView.h"
+//#import "RCTUtils.h"
+//#import "RCTConvert.h"
